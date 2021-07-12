b8f262d14dcd085af4d4e2d8f0076c45e9533c85
FrancoRosa <km115.franco@gmail.com>
2021-07-11 20:14:45 -0500

    Add file input component

--- src/components/FileInput.jsx
+++ src/components/FileInput.jsx
@@ -1,13 +1,30 @@
+import { faUpload } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { useEffect, useState } from 'react';
+
 const FileInput = () => {
-  const name = 'pablito';
+  const [path, setPath] = useState('');
+  const [name, setName] = useState('Nothing selected yet');
+
+  useEffect(()=>{
+    const handleFiles = e => {
+      let localPath = URL.createObjectURL(e.target.files[0]);
+      let localName = e.target.files[0].name
+      setPath(localPath)
+      setName(localName)
+    }
+
+    const inputElement = document.querySelector(".file-input");
+    inputElement.addEventListener("change", handleFiles, false);
+  },[])
 
   return (
     <div className="file has-name is-large">
       <label className="file-label">
-        <input className="file-input" type="file" />
+        <input className="file-input" type="file" accept=".csv"/>
         <span className="file-cta">
           <span className="file-icon">
-            <i className="fas fa-upload"></i>
+          <FontAwesomeIcon icon={faUpload} />
           </span>
           <span className="file-label">
             Choose a file ...
