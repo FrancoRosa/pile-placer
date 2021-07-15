dbaa837879bc9b6963330b160dda2bb18057e715
FrancoRosa <km115.franco@gmail.com>
2021-07-15 14:32:29 -0500

    Add google maps to project

--- src/components/UserMap.jsx
+++ src/components/UserMap.jsx
@@ -1,26 +1,22 @@
-import { faUpload } from '@fortawesome/free-solid-svg-icons';
-import { MapContainer, TileLayer, Popup } from 'react-leaflet'
-import Marker from 'react-leaflet-enhanced-marker'
-import 'leaflet/dist/leaflet.css'
+import {Map, Marker, GoogleApiWrapper, Polyline} from 'google-maps-react';
 
-const UserMap = () =>{
+const UserMap = ({google}) =>{
   const location = {
     lat: -13.21, 
     lng: -72.1
   }
-  return(
-    <MapContainer center={location} zoom={13} scrollWheelZoom={false}>
-      <TileLayer
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a>'
-      />
-    <Marker position={location} icon={<img src={faUpload}/>}>
-      <Popup>
-        A pretty CSS3 popup. <br /> Easily customizable.
-      </Popup>
-    </Marker>
-  </MapContainer>
+
+  return (
+    <div className="container map">
+      <Map google={google} zoom={18} 
+        initialCenter={location}
+      >
+        <Marker onClick={()=>console.log('click')} position={location}/>
+      </Map>
+    </div>
   );
 };
 
-export default UserMap;
+export default GoogleApiWrapper({
+  apiKey: ('AIzaSyBsgd5A9q-23gHy8tL6e5O0lct6JoD97xo')
+})(UserMap)
