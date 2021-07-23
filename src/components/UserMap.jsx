8a31f47b21b43c8080a936700c16e802845359ad
FrancoRosa <km115.franco@gmail.com>
2021-07-23 18:35:44 -0500

    Add chnage heading rutine

--- src/components/UserMap.jsx
+++ src/components/UserMap.jsx
@@ -2,10 +2,11 @@ import {Map, Marker, GoogleApiWrapper, Polyline} from 'google-maps-react';
 import { useEffect, useState } from 'react';
 import { socket } from '../js/api';
 
-const UserMap = ({ google }) =>{
+const UserMap = ({ google, map }) =>{
   const [center, setCenter] = useState({ heading: 0, lat: 0, lng: 0 })
   
   useEffect(() => {
+    
     socket.on('message', msg => {
       msg = JSON.parse(msg)
       console.log(msg);
@@ -16,6 +17,10 @@ const UserMap = ({ google }) =>{
       socket.off('message');
     };
   }, [])
+
+  const changeHeading = map => {
+    map.setHeading(180);
+  }
 
   const get_icon = color => ({
     path: "M10.453 14.016l6.563-6.609-1.406-1.406-5.156 5.203-2.063-2.109-1.406 1.406zM12 2.016q2.906 0 4.945 2.039t2.039 4.945q0 1.453-0.727 3.328t-1.758 3.516-2.039 3.070-1.711 2.273l-0.75 0.797q-0.281-0.328-0.75-0.867t-1.688-2.156-2.133-3.141-1.664-3.445-0.75-3.375q0-2.906 2.039-4.945t4.945-2.039z",
@@ -57,7 +62,8 @@ const UserMap = ({ google }) =>{
   return (
     <div className="container map">
       <Map google={google} zoom={16} 
-        initialCenter={center} center={center}
+        initialCenter={center} center={center} heading={center.heading}
+        onClick={changeHeading}
       >
         {waypoints.map(waypoint => (
           <Marker onClick={()=>console.log('click')} 
