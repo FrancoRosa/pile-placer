83c13f548a28ad726e2187a619e7bc2b442b6182
FrancoRosa <km115.franco@gmail.com>
2021-08-20 16:37:12 -0500

    Add audio feedback

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,15 +1,18 @@
 import UserMap from "./UserMap";
-import { useContext, useEffect } from "react";
-import { WayPointContext } from "../js/WayPointContext";
-// import { playDistance, playColor } from "../js/audio";
 import NextPile from "./NextPile";
+import { useStoreState } from "easy-peasy";
+import { useEffect } from "react";
+import { playDistance } from "../js/audio";
 
 const Home = () => {
-  // const waypoint = useContext(WayPointContext)
-
-  // useEffect(()=>{
-  //   playDistance(waypoint.waypoint.distance)
-  // },[waypoint])
+  const nextPiles = useStoreState(state => state.nextPiles)
+  
+  useEffect(()=>{
+    if(nextPiles[0].distance) {
+      let minDistance = Math.min(nextPiles[0].distance, nextPiles[1].distance)
+      playDistance(minDistance)
+    }
+  },[nextPiles])
 
   return (
     <div className="container">
