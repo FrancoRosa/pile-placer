b1c487bd452c2ad1e56b51f28fd9cd4814742008
FrancoRosa <km115.franco@gmail.com>
2021-09-09 18:13:38 -0500

    Process incoming json to move servos

--- avr/laserpointer/laserpointer.ino
+++ avr/laserpointer/laserpointer.ino
@@ -1,3 +1,28 @@
+/*
+  laserpointer
+  This code is made to handle two pan/tilt modules and lasers attached
+  to them.
+  To change the rotation angles and laser behaviour user must send 
+  serial commands like this:
+  {
+    turrets: [
+      { 
+        base:0,
+        top:0,
+        laser: false
+      }, 
+      { 
+        base:90,
+        top:90,
+        laser: true
+      },
+    ]
+  } 
+
+  Serialized version:
+  {turrets:[{base:45,top:90,laser:false},{base:135,top:180,laser:true}]} 
+*/
+
 #include <Servo.h>
 #include <ArduinoJson.h>
 
@@ -9,10 +34,68 @@
 #define s1laser 7
 #define s2laser 11
 
+#define jsonBufferSize 200
+char jsonBuffer[jsonBufferSize];
+int json_i = 0;
+
 Servo servo1base;
 Servo servo1top;
 Servo servo2base;
 Servo servo2top;
+
+StaticJsonDocument<jsonBufferSize> doc;
+
+volatile int base1 = 90;
+volatile int top1 = 90;
+volatile int laser1 = 0;
+volatile int base2 = 90;
+volatile int top2 = 90;
+volatile int laser2 = 0;
+
+volatile bool flagProcessed = false;
+
+void getJson(char c)
+{
+  jsonBuffer[json_i] = c;
+  json_i++;
+  if (json_i > jsonBufferSize)
+    json_i = 0;
+  if ((json_i >= 2) && ((c == '\n') || (c == '\r')))
+  {
+    jsonBuffer[json_i] = '\0';
+    getValues();
+    clearBuffer();
+  }
+}
+
+void clearBuffer()
+{
+  json_i = 0;
+  for (int i = 0; i < jsonBufferSize; i++)
+    jsonBuffer[i] = 0;
+}
+
+void getValues()
+{
+  DeserializationError error = deserializeJson(doc, jsonBuffer);
+
+  if (error)
+  {
+    Serial.print("deserializeJson() failed: ");
+    Serial.println(error.f_str());
+    return;
+  }
+  else
+  {
+    base1 = doc["turrets"][0]["base"];
+    top1 = doc["turrets"][0]["top"];
+    laser1 = doc["turrets"][0]["laser"];
+    base2 = doc["turrets"][1]["base"];
+    top2 = doc["turrets"][1]["top"];
+    laser2 = doc["turrets"][1]["laser"];
+    flagProcessed = true;
+  }
+}
 
 void setup()
 {
@@ -25,12 +108,30 @@ void setup()
   pinMode(led, OUTPUT);
 
   Serial.begin(9600);
+  while (!Serial)
+    continue;
   Serial.println("... start");
+  updateActuators();
+}
+
+void updateActuators()
+{
+  servo1base.write(base1);
+  servo1top.write(top1);
+  servo2base.write(base2);
+  servo2top.write(top2);
+  digitalWrite(s1laser, laser1);
+  digitalWrite(s2laser, laser2);
 }
 
 void loop()
 {
-  digitalWrite(led, !digitalRead(led));
-  delay(100);
-  Serial.print('.');
+  if (Serial.available())
+    getJson(Serial.read());
+  if (flagProcessed)
+  {
+    updateActuators();
+    Serial.println("... ok");
+    flagProcessed = false;
+  }
 }
