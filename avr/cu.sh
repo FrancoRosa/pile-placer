571212402ca31592c4e4646a3c635ed2449e9601
FrancoRosa <km115.franco@gmail.com>
2021-09-14 04:07:25 +0100

    Update compiler code

--- avr/cu.sh
+++ avr/cu.sh
@@ -8,14 +8,8 @@
 
 #  Arbotix commands
  echo '... compile'
-<<<<<<< HEAD
  arduino-cli compile --fqbn arbotix:avr:arbotix dynapointer
  echo '... upload'
  arduino-cli upload --port /dev/ttyUSB0 --fqbn arbotix:avr:arbotix dynapointer
-=======
- arduino-cli compile --fqbn arbotix:avr:arbotix laserpointer
- echo '... upload'
- arduino-cli upload --port /dev/ttyUSB0 --fqbn arbotix:avr:arbotix laserpointer
->>>>>>> 55d0bab8ede17689f8d23758b7ef3677215ab2c1
  echo '... done'
  
