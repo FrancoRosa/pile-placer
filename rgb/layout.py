c743af28bcf44576f691c9010262c50e60f03f1e
FrancoRosa <km115.franco@gmail.com>
2021-09-28 19:31:13 +0100

    Layout blocks

--- rgb/layout.py
+++ rgb/layout.py
@@ -9,31 +9,66 @@ class RunText(SampleBase):
         super(RunText, self).__init__(*args, **kwargs)
         self.parser.add_argument("-t", "--text", help="The text to scroll on the RGB LED panel", default="Hello world!")
 
+    
+
     def run(self):
         offscreen_canvas = self.matrix.CreateFrameCanvas()
         font = graphics.Font()
         font.LoadFont("fonts/7x13.bdf")
         fontBig = graphics.Font()
         fontBig.LoadFont("fonts/10x20.bdf")
+        font_small = graphics.Font()
+        font_small.LoadFont("fonts/4x6.bdf")
         pos = offscreen_canvas.width
         my_text = self.args.text
 
+        def left_block(color, text):
+            graphics.DrawText(offscreen_canvas, font_small, 1, 30, colors[color], text)
+            graphics.DrawText(offscreen_canvas, fontBig, 1, 18, colors[color], "\u2593")
+            graphics.DrawText(offscreen_canvas, font_small, 1, 6, colors['black'], 'next')
+        
+        def right_block(color, text):
+            graphics.DrawText(offscreen_canvas, font_small, 116, 30, colors[color], text)
+            graphics.DrawText(offscreen_canvas, fontBig, 116, 18, colors[color], "\u2593")
+            graphics.DrawText(offscreen_canvas, font_small, 116, 6, colors['black'], 'next')
+
+
+        speed = 0.25
+
         while True:
-            offscreen_canvas.Clear()
-            graphics.DrawText(offscreen_canvas, font, 50, 15, colors['white'], "Hello ")
-            graphics.DrawText(offscreen_canvas, font, 35, 25, colors['lightblue'], "Mortenson")
-            graphics.DrawText(offscreen_canvas, fontBig, 0, 15, colors['red'], "<<")
-            graphics.DrawText(offscreen_canvas, fontBig, 0, 29, colors['red'], "<<")
-            graphics.DrawText(offscreen_canvas, fontBig, 108, 15, colors['yellow'], ">>")
-            graphics.DrawText(offscreen_canvas, fontBig, 108, 29, colors['yellow'], ">>")
-
-
-            time.sleep(0.05)
+            
             offscreen_canvas = self.matrix.SwapOnVSync(offscreen_canvas)
+            offscreen_canvas.Clear()
+            # graphics.DrawText(offscreen_canvas, font, 50, 15, colors['white'], "Hello ")
+            # graphics.DrawText(offscreen_canvas, font, 35, 25, colors['lightblue'], "Mortenson")
+            left_block('red', ' <<')
+            right_block('white', ' >>')
+            time.sleep(speed)
+
+            offscreen_canvas = self.matrix.SwapOnVSync(offscreen_canvas)
+            offscreen_canvas.Clear()
+            # graphics.DrawText(offscreen_canvas, font, 50, 15, colors['white'], "Hello ")
+            # graphics.DrawText(offscreen_canvas, font, 35, 25, colors['lightblue'], "Mortenson")
+            left_block('red', '< <')
+            right_block('white', '> >')
+            time.sleep(speed)
+
+            offscreen_canvas = self.matrix.SwapOnVSync(offscreen_canvas)
+            offscreen_canvas.Clear()
+            # graphics.DrawText(offscreen_canvas, font, 50, 15, colors['white'], "Hello ")
+            # graphics.DrawText(offscreen_canvas, font, 35, 25, colors['lightblue'], "Mortenson")
+            left_block('red', '<< ')
+            right_block('white', '>> ')
+            time.sleep(speed)
+
+# @app.route('/')
+# def index():
+#     return "... source server running on port %s" % port
 
 
 # Main function
 if __name__ == "__main__":
     run_text = RunText()
+    # app.run(debug=False, port=port, host='0.0.0.0')
     if (not run_text.process()):
         run_text.print_help()
