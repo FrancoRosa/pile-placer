a6fb1d1b185a100cfd1e5a24cf359b6622a70bce
FrancoRosa <km115.franco@gmail.com>
2021-09-28 17:26:18 +0100

    Add color codes

---
+++ rgb/colormap.py
@@ -0,0 +1,15 @@
+from rgbmatrix import graphics
+
+colors = {
+    'black': graphics.Color(0,0,0)
+    'brown': graphics.Color(0,0,0)
+    'darkblue': graphics.Color(0,0,0)
+    'lightblue': graphics.Color(0,0,0)
+    'lightgreen': graphics.Color(0,0,0)
+    'orange': graphics.Color(0,0,0)
+    'pink': graphics.Color(0,0,0)
+    'purple': graphics.Color(0,0,0)
+    'red': graphics.Color(0,0,0)
+    'white': graphics.Color(0,0,0)
+    'yellow': graphics.Color(0,0,0)
+}
