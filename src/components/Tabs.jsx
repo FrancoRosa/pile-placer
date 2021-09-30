833c1503a1d7f4d36987a236c2fcc37aad529a3b
FrancoRosa <km115.franco@gmail.com>
2021-09-30 11:54:39 -0500

    Add debug view

--- src/components/Tabs.jsx
+++ src/components/Tabs.jsx
@@ -1,18 +1,16 @@
-import { 
-  faTools,
-  faMapMarked
-} from '@fortawesome/free-solid-svg-icons';
-import Tab from './Tab';
+import { faTools, faMapMarked, faBug } from "@fortawesome/free-solid-svg-icons";
+import Tab from "./Tab";
 
 const Tabs = () => {
-    return (
+  return (
     <div className="tabs is-centered m-0">
       <ul>
         <Tab name="Map" icon={faMapMarked} />
         <Tab name="Config" icon={faTools} />
+        <Tab name="Debug" icon={faBug} />
       </ul>
     </div>
-  )
-}
+  );
+};
 
 export default Tabs;
